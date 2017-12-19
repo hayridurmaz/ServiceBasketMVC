8bc84624530f5f382c16e9407d40cd6c58e5a536
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2017-12-19 23:42:41 +0300

    escape conflict

--- ServiceBasket/Controllers/ServiceController.cs
+++ ServiceBasket/Controllers/ServiceController.cs
@@ -35,13 +35,6 @@ namespace ServiceBasket.Controllers
         }
 
         [HttpGet]
-        public ActionResult AddComment()
-        {
-            return View(new Comment());
-        }
-
-
-        [HttpGet]
         public ActionResult AddService()
         {
             return View(new Service());
@@ -90,5 +83,48 @@ namespace ServiceBasket.Controllers
             }
             
         }
+
+        [HttpGet]
+        public ActionResult AddComment(Service service)
+        {
+            Comment comment = new Comment();
+            comment.Service = ServicePersistence.GetService(service.Title);
+            return View(comment);
+        }
+
+        [HttpPost]
+        public ActionResult AddComment(Comment comment)
+        {
+            if (Session["userId"] == null)
+            {
+                TempData["commentAdded"] = "Please Log in.";
+                return View(comment);
+            }
+            comment.Writer = UserPersistence.GetUser(Session["userId"].ToString());
+            comment.CommentId = CommentPersistence.getMaxId() + 1;
+
+            bool? acceptible = false;
+            acceptible = CommentManager.AddNewComment(comment);
+            if ((acceptible != null))
+            {
+
+                if (acceptible == true)
+                {
+                    TempData["commentAdded"] = "Comment is added successfully.";
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["commenteAdded"] = "Comment could not be added.";
+                    return View(comment);
+                }
+            }
+            else
+            {
+                TempData["commentAdded"] = "Comment could not be added.";
+                return View(comment);
+            }
+        }
+
     }
 }
